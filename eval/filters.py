ae4b73c1d80e4b3d0f27d1ee53d7957fa10107fe
sibyl <fabriceyhc@gmail.com>
2023-01-14 12:52:33 -0800

    Update filters.py

--- eval/filters.py
+++ eval/filters.py
@@ -37,6 +37,18 @@ class CleanLabFilter:
         output = self.pipe(dataset['text'])
         return np.stack([vectorize(o) for o in output])
 
+    def find_num_to_remove_per_class(self, dataset, frac_to_remove=0.1):
+        classes = dataset.features['label'].names
+        num_classes = len(classes)
+        print(num_classes)
+
+        num_per_class = []
+        for i in range(num_classes):
+            class_partition = dataset.filter(lambda row: row["label"] == i)
+            num_per_class.append(len(class_partition))
+        num_to_remove_per_class = [int(frac_to_remove * num) for num in num_per_class]
+        return num_to_remove_per_class
+
     def clean_dataset(self, dataset):
         if self.pipe is None:
             return dataset
@@ -46,11 +58,20 @@ class CleanLabFilter:
 
         pred_probs = self.extract_prediction_probabilities(dataset)
         print(f"pred_probs.shape ({pred_probs.shape})")
+
+        num_to_remove_per_class = self.find_num_to_remove_per_class(dataset)
+        print(f"num_to_remove_per_class ({num_to_remove_per_class})")
+
+        num_to_add = pred_probs.shape[-1] - len(num_to_remove_per_class)
+        print(f"num_to_add: {num_to_add}")
+        for i in range(num_to_add):
+            num_to_remove_per_class.append(0)
+
         suss_idx = find_label_issues(
             labels=dataset['label'],
             pred_probs=pred_probs,  
             return_indices_ranked_by='self_confidence',
-            min_examples_per_class=(dataset_len // num_classes) - 1
+            num_to_remove_per_class = num_to_remove_per_class
         )
         print(f"suss_idx.len ({len(suss_idx)})")
         idx_to_keep = [i for i in range(len(dataset)) if i not in suss_idx]
